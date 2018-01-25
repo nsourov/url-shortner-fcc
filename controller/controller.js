a99fe5e79ae836a35d5673a6862880c926a4394a
nsourov <naimur.rahman103@gmail.com>
2018-01-25 23:28:30 +0600

    make it better

--- controller/controller.js
+++ controller/controller.js
@@ -39,8 +39,9 @@ const urlToShort = (req, res) => {
 
 const shortenedUrl = (req, res) => {
   const { shortened_url } = req.params;
+  console.log({"a": shortened_url})
   const short_url = `https://short-url-mic-fcc.herokuapp.com/${shortened_url}`;
-  console.log(short_url)
+  console.log({"b": short_url})
   UrlToShort.findOne({ shortened_url: short_url }, (err, data) => {
     if (err) {
       res.json(err);
