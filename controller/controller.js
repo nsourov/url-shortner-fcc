e744a656fb3554a68db0d68c627aace852c76f77
nsourov <naimur.rahman103@gmail.com>
2018-01-26 00:55:12 +0600

    make it better

--- controller/controller.js
+++ controller/controller.js
@@ -39,19 +39,19 @@ const urlToShort = (req, res, next) => {
   }
 };
 
-const shortenedUrl = (req, res,next) => {
-  const splited = req.params[0].split('/')
-  const urlToForward = splited[1]
-  next()
-  console.log(urlToForward)
-  const short_url = `https://short-url-mic-fcc.herokuapp.com/${urlToForward}`;
-  console.log(short_url)
-  UrlToShort.findOne({ 'shortened_url': short_url }, (err, data) => {
-    if (err) {
-      res.json(err);
-    }
-   res.redirect(301, data.original_url);
-  });
-};
+// const shortenedUrl = (req, res,next) => {
+//   const splited = req.params[0].split('/')
+//   const urlToForward = splited[1]
+//   next()
+//   console.log(urlToForward)
+//   const short_url = `https://short-url-mic-fcc.herokuapp.com/${urlToForward}`;
+//   console.log(short_url)
+//   UrlToShort.findOne({ 'shortened_url': short_url }, (err, data) => {
+//     if (err) {
+//       res.json(err);
+//     }
+//    res.redirect(301, data.original_url);
+//   });
+// };
 
-module.exports = {urlToShort, shortenedUrl}
+module.exports = {urlToShort}
--- router/router.js
+++ router/router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controller/controller.js')
 
 router.get('/new/*', controller.urlToShort)
-router.get('/:urlToForward*', controller.shortenedUrl)
+// router.get('/:urlToForward*', controller.shortenedUrl)
 
 module.exports = router;
