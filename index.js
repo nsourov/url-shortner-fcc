22414ea6f250c615fc3a1982c7446cedc822ebb0
nsourov <naimur.rahman103@gmail.com>
2018-01-25 23:37:51 +0600

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const router = require('./router/router')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-require("dotenv").config();
 app.listen(process.env.PORT || 3000, () => console.log("Server running"));
 mongoose.Promise = global.Promise;
 mongoose.connect(
